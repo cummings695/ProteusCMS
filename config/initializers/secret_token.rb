9fe7e04c99e88af3cafc6a7a5a4d11900c350bba
Matthew Cummings <cummings695@gmail.com>
2014-02-22 09:50:23 -0500

    initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProteusCMS::Application.config.secret_key_base = 'd6fdcfcbfd4b641a05a3e459312af767c7aee6cf727b3e7dbd952420cae9319fc69daaf4a1b14ffe8cd9b852d647633fb7a0b65af9ff20005d18518f9044269a'
